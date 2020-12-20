b82ac3116242fbdf237e14c0ed96677c2884d3c1
sharegoat98 <aleksa.berdih@gmail.com>
2020-12-20 19:57:02 +0100

    malo smo guglali

---
+++ SBESProjekat/CertificateManagerService/DataCertificate.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CertificateManagerService
+{
+    public sealed class DataCertificate
+    {
+        #region Generate a certificate
+        /// <summary>     
+        /// Generate a certificate based on the specified certificate name and makecert full path (including the public and private keys and save them in the MY store)     
+        /// </summary>     
+        /// <param name="subjectName"></param>     
+        /// <param name="makecertPath"></param>     
+        /// <returns></returns>
+        //The parameter is: makecert -r -pe -n "cn=MyCA" -$ commercial -a sha1 -b 08/05/2010 -e 01/01/2012 -cy authority -ss my -sr currentuser
+        //The meaning of each part:
+        //-r: self-signed
+        //-pe: Marks the generated private key as exportable. This will include the private key in the certificate.
+        //-n "cn=MyCA": The subject name of the certificate, the .net comes with the X509Store class in the library, which can be found in the store according to the certificate subject name.
+        //store reference: X509Store class 
+        //-$ commercial: Indicates the commercial use of the certificate. . .
+        //-a: Specify the signature algorithm. Must be md5 (default) or sha1.
+        //-b 08/05/2010: The start time of the certificate validity period, the default is the date the certificate was created. The format is: mm/dd/yyyy
+        //-e 01/01/2012: Specify the end time of the validity period. The default is 12/31/2039 11:59:59 GMT. Same format as above
+        //-ss my: The certificate is generated to my personal store area
+        //-sr currentuser: Keeps the current personal user area of ​​the computer. Other users cannot see the certificate after logging in to the system. .
+        public static bool CreateCertWithPrivateKey(string subjectName, string makecertPath)
+        {
+            subjectName = "CN=" + subjectName;
+            //string param = " -pe -ss my -n \"" + subjectName + "\" ";
+            string param = " -r -pe -ss my -n \"" + subjectName + "\" " + " -a sha256 -b 01/01/2019 -e 01/01/4019";
+            try
+            {
+                Process p = Process.Start(makecertPath, param);
+                p.WaitForExit();
+                p.Close();
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+            return true;
+        }
+        #endregion
+
+
+        /// <summary>     
+        /// Find the certificate with the subject subjectName from the personal MY area of ​​the WINDOWS certificate store.     
+        /// and export to a pfx file with a password for it     
+        /// and remove the certificate from the personal area (if isDelFromstor is true)     
+        /// </summary>     
+        /// <param name="subjectName">Certificate subject, excluding CN=</param>     
+        /// <param name="pfxFileName">pfx filename</param>     
+        /// <param name="password">pfx file password</param>     
+        /// <param name="isDelFromStore">Remove from storage</param>     
+        /// <returns></returns>     
+        public static bool ExportToPfxFile(string subjectName, string pfxFileName, string password, bool isDelFromStore)
+        {
+            subjectName = "CN=" + subjectName;
+            X509Store store = new X509Store(StoreName.My, StoreLocation.CurrentUser);
+            store.Open(OpenFlags.ReadWrite);
+            X509Certificate2Collection storecollection = (X509Certificate2Collection)store.Certificates;
+            foreach (X509Certificate2 x509 in storecollection)
+            {
+                if (x509.Subject == subjectName)
+                {
+                    Debug.Print(string.Format("certificate name: {0}", x509.Subject));
+
+                    byte[] pfxByte = x509.Export(X509ContentType.Pfx, password);
+                    using (FileStream fileStream = new FileStream(pfxFileName, FileMode.Create))
+                    {
+                        // Write the data to the file, byte by byte.     
+                        for (int i = 0; i < pfxByte.Length; i++)
+                            fileStream.WriteByte(pfxByte[i]);
+                        // Set the stream position to the beginning of the file.     
+                        fileStream.Seek(0, SeekOrigin.Begin);
+                        // Read and verify the data.     
+                        for (int i = 0; i < fileStream.Length; i++)
+                        {
+                            if (pfxByte[i] != fileStream.ReadByte())
+                            {
+                                fileStream.Close();
+                                return false;
+                            }
+                        }
+                        fileStream.Close();
+                    }
+                    if (isDelFromStore == true)
+                        store.Remove(x509);
+                }
+
+            }
+
+            store.Close();
+            store = null;
+            storecollection = null;
+            return true;
+        }
+
+
+
+        public static X509Certificate2 GetCertificateFromPfxFile(string pfxFileName, string password)
+        {
+            try
+            {
+                return new X509Certificate2(pfxFileName, password, X509KeyStorageFlags.Exportable);
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+        }
+
+
+        public static X509Certificate2 GetCertificateFromStore(string subjectName)
+        {
+            subjectName = "CN=" + subjectName;
+            X509Store store = new X509Store(StoreName.My, StoreLocation.CurrentUser);
+            store.Open(OpenFlags.ReadWrite);
+            X509Certificate2Collection storecollection = (X509Certificate2Collection)store.Certificates;
+            foreach (X509Certificate2 x509 in storecollection)
+            {
+                if (x509.Subject == subjectName)
+                {
+                    return x509;
+                }
+            }
+            store.Close();
+            store = null;
+            storecollection = null;
+            return null;
+        }
+
+        /// <summary>     
+        /// Return the certificate entity based on the public key certificate     
+        /// </summary>     
+        /// <param name="cerPath"></param>     
+        public static X509Certificate2 GetCertFromCerFile(string cerPath)
+        {
+            try
+            {
+                return new X509Certificate2(cerPath);
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+        }
+
+
+        public static bool ExportToCerFile(string subjectName, string cerFileName)
+        {
+            subjectName = "CN=" + subjectName;
+            X509Store store = new X509Store(StoreName.My, StoreLocation.CurrentUser);
+            store.Open(OpenFlags.ReadWrite);
+            X509Certificate2Collection storecollection = (X509Certificate2Collection)store.Certificates;
+            foreach (X509Certificate2 x509 in storecollection)
+            {
+                if (x509.Subject == subjectName)
+                {
+                    Debug.Print(string.Format("certificate name: {0}", x509.Subject));
+                    //byte[] pfxByte = x509.Export(X509ContentType.Pfx, password);     
+                    byte[] cerByte = x509.Export(X509ContentType.Cert);
+                    using (FileStream fileStream = new FileStream(cerFileName, FileMode.Create))
+                    {
+                        // Write the data to the file, byte by byte.     
+                        for (int i = 0; i < cerByte.Length; i++)
+                            fileStream.WriteByte(cerByte[i]);
+                        // Set the stream position to the beginning of the file.     
+                        fileStream.Seek(0, SeekOrigin.Begin);
+                        // Read and verify the data.     
+                        for (int i = 0; i < fileStream.Length; i++)
+                        {
+                            if (cerByte[i] != fileStream.ReadByte())
+                            {
+                                fileStream.Close();
+                                return false;
+                            }
+                        }
+                        fileStream.Close();
+                    }
+                }
+            }
+            store.Close();
+            store = null;
+            storecollection = null;
+            return true;
+        }
+
+
+
+    }
+}
--- SBESProjekat/CertificateManagerService/Program.cs
+++ SBESProjekat/CertificateManagerService/Program.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,16 +11,74 @@ namespace CertificateManagerService
 {
     class Program
     {
+        static string RSADecrypt(string xmlPrivateKey, string m_strDecryptString)
+        {
+            RSACryptoServiceProvider provider = new RSACryptoServiceProvider();
+            provider.FromXmlString(xmlPrivateKey);
+            byte[] rgb = Convert.FromBase64String(m_strDecryptString);
+            byte[] bytes = provider.Decrypt(rgb, false);
+            return new UnicodeEncoding().GetString(bytes);
+        }
+        /// <summary>     
+        /// RSA encryption     
+        /// </summary>     
+        /// <param name="xmlPublicKey"></param>     
+        /// <param name="m_strEncryptString"></param>     
+        /// <returns></returns>     
+        static string RSAEncrypt(string xmlPublicKey, string m_strEncryptString)
+        {
+            RSACryptoServiceProvider provider = new RSACryptoServiceProvider();
+            provider.FromXmlString(xmlPublicKey);
+            byte[] bytes = new UnicodeEncoding().GetBytes(m_strEncryptString);
+            return Convert.ToBase64String(provider.Encrypt(bytes, false));
+        }
+
         static void Main(string[] args)
         {
-            string cert = Console.ReadLine();
-            string cmdText = "makecert - sv "+cert+".pvk"+" - iv TestCA.pvk - n \"CN=wcfservice\" - pe - ic TestCA.cer"+cert+".cer" +" - sr localmachine - ss My - sky exchange";
-            string proba = "mkdir radi";
+            String certificateName = "";//Certificate name
+            string keyPublic = "";
+            string keyPrivate = "";
+            certificateName = "cao4";
+            //X509Certificate2 privateCert = new X509Certificate2(pfxFilePath, pfxPassword, X509KeyStorageFlags.Exportable);  
+            //// Create a certificate in the personal certificate directory in the local store
+            ////makecert Win10 corresponds to the directory C:\Program Files (x86)\Windows Kits\8.0\bin\x64, different versions of Windows makecert directory may be different           
+            DataCertificate.CreateCertWithPrivateKey(certificateName, "C:\\Program Files (x86)\\Windows Kits\\10\\bin\\10.0.17763.0\\x86\\makecert.exe");
+            //Get the certificate
+            X509Certificate2 c1 = DataCertificate.GetCertificateFromStore(certificateName);
+            keyPublic = c1.PublicKey.Key.ToXmlString(false); // public key  
+            keyPrivate = c1.PrivateKey.ToXmlString(true); // private key
+            String cypher = RSAEncrypt(keyPublic, "programmer"); // Encryption  
+            String plain = RSADecrypt(keyPrivate, cypher); // decrypt  
 
+            System.Diagnostics.Debug.Assert(plain == "programmer");
+            ////First time Read the generated cer certificate from the personal certificate directory in the local store Copy to the specified folder 
+            DataCertificate.ExportToCerFile(certificateName, "d:\\Aleksa\\" + certificateName + ".cer");
+            X509Certificate2 c2 = DataCertificate.GetCertFromCerFile("d:\\Aleksa\\" + certificateName + ".cer");
+            string keyPublic2 = c2.PublicKey.Key.ToXmlString(false);
+            bool b = keyPublic2 == keyPublic;
+            if (!b)
+                keyPublic2 = keyPublic;
+            String cypher2 = RSAEncrypt(keyPublic2, "Programmer 2"); // Encryption  
+            String plain2 = RSADecrypt(keyPrivate, cypher2); // Decrypt, there is no private key in cer, so use the private key obtained earlier to decrypt 
 
-           
-            
+            //Generate a certificate to get the public key private key to re-call after encryption and decryption
+            ////X509Certificate2 c2 = DataCertificate.GetCertFromCerFile("d:\\mycert\\" + certificateName + ".cer");
+            //keyPublic = "<RSAKeyValue><Modulus>tZ6JUEO8A1gPE0+b2/QdxxgDmaKJvwD9hwXmxRaUc6OKlZ4QRNliLCDMvrgaXPF2S3QiqGmQnoeNPWF90IsaB/wxc4ayglEX8wl2yV8PARcQhx+V+wIJsBD0vVNk3sMBAfZrVrVJOZNkjUcAFmPYnPxkq/7y4HJFG+TCRNMVsRjreywK+CLrZFnYvUiw/xbNA6fJ03nkFDNoPsXAXVOUJv9J8FXiTirPx21aGJddy7AFlvNytgOru5F6pJYI76fe5YYGYYUVHjgIOCmkjcolwYt6AUmlmKG1bsGvd0Imacf5rKu32E8J8avcojnKyqpOGOPssMxq3/s0YsX6yJfHtQ==</Modulus><Exponent>AQAB</Exponent></RSAKeyValue>";
+            //keyPrivate = "<RSAKeyValue><Modulus>tZ6JUEO8A1gPE0+b2/QdxxgDmaKJvwD9hwXmxRaUc6OKlZ4QRNliLCDMvrgaXPF2S3QiqGmQnoeNPWF90IsaB/wxc4ayglEX8wl2yV8PARcQhx+V+wIJsBD0vVNk3sMBAfZrVrVJOZNkjUcAFmPYnPxkq/7y4HJFG+TCRNMVsRjreywK+CLrZFnYvUiw/xbNA6fJ03nkFDNoPsXAXVOUJv9J8FXiTirPx21aGJddy7AFlvNytgOru5F6pJYI76fe5YYGYYUVHjgIOCmkjcolwYt6AUmlmKG1bsGvd0Imacf5rKu32E8J8avcojnKyqpOGOPssMxq3/s0YsX6yJfHtQ==</Modulus><Exponent>AQAB</Exponent><P>1tSbdgI4XdOdZGbPgwSOWqp0FzRe1GaKB/6zBtXVjR2iehF8BhrWbiYL2WGz2BgGQT7kyA240keywKzihmm7F+K0L0BTb/6t8ZIyFvIfj3u41P5BJ5ikQ/6ZLVBC2ZvrQwHz6ycxAt8gPbJPndfAjc3OMUQmBvalo9PUwGkX3FM=</P><Q>2Gyd1Pioi6QiDM/cdgCB97nkh/GlcnMlsBp654YheoTiaq/AiEeaNwQiSRsFmzWU5PsEKoUpOtKadCqhdSqvU47OdU+gD7aXyLgsIVbtLWdzFX4Bq06a7P1fYN2HPvOjPec2oEU6HRX9/XBJzXAGslw3JFUZlhWJRajbk6Lzitc=</Q><DP>0ft6APzmj39aJlr/lfaMFj7pvgyobD/Vxz7DSnkUhRxkRaB1c5oj4gI6Lr57BUtmQbvx70DKWG9QX1gdCniqMQycRls/swZiiu71GsyK4LpzzWy/zq46UWO34TzEOuNWL2bnPgBOvZnOb7+sZoIOagyx8CHGcaP//4P8Ph36/pU=</DP><DQ>OfxIAWKqDdfpA5PBlqAmMlBNCZtV36c4Rsmhely2pZPq8fiq1hiRGgJyiTHDO8WMYhlbEWViGY+JsGwnnDPWi8WsTUQLN4qNekrWEAyxOUQJUo3TNqm12p88KcDQ1q4CY7iKK0DBBD/7MCcgrvk/4hPQ9lwSoeKdR9upERJMvDs=</DQ><InverseQ>TMnaTY0Qcg5qhEB1+Mb5/QPDQzOpO+tlduALO23Kuubm+lYQ5sZUyHGv25lb0RAUkFqDFeyA0M9Y8Y8lCuXkjzU6depFZHay9a9OG8WJ7g5AcSuNltjbqC2y542dgnBU9gzUogySrVGAHdBFkQhwukH6tSgqIVR4PblI3Vr54uQ=</InverseQ><D>L2nM5SRZr/HMNblhsgE/yNsPDYuuNCv5A8fZn/guFyZJppeWHbM2etixOtTrJPpwbHBMH/U3KPuwNqb95nR5/j2rV0KB1Z2ACBWfaiCj1SAFU5E+YUH973XtvoNH4RO9bpq7GO7Ix/wfkvZHIpE8WndVfMVY+Jk8S3Tj9n24uvua4HHsBxy1TeRdRHojWWVZm79aH+CFhGIemSHSVEKp+GfPhIUzMGXwfA6l4M4FSpb+XNbB7un29JjTNB8TV49qVY4QelpsS/J9v6eMg0GDYBJIcG0AkghrFWBGjVyHGUJJsZhGVYAxdJow4BWo+Y129LYtcZtznGhH4VsjpTNsfQ==</D></RSAKeyValue>";
+            //string cypher2 = RSAEncrypt(keyPublic, "Programmer 2"); // Encryption  
+            //string plain2 = RSADecrypt(keyPrivate, cypher2); // Decrypt, there is no private key in cer, so use the private key obtained earlier to decrypt 
 
+            //System.Diagnostics.Debug.Assert(plain2 == "programmer 2");
+            // generate a pfx and delete it from the store
+            DataCertificate.ExportToPfxFile(certificateName, "d:\\Aleksa\\" + certificateName + ".pfx", "111", true);
+            X509Certificate2 c3 = DataCertificate.GetCertificateFromPfxFile("d:\\Aleksa\\" + certificateName + ".pfx", "111");
+
+            String keyPublic3 = c3.PublicKey.Key.ToXmlString(false); // public key  
+            String keyPrivate3 = c3.PrivateKey.ToXmlString(true); // private key  
+
+            String cypher3 = RSAEncrypt(keyPublic3, "programmer 3"); // encryption  
+            String plain3 = RSADecrypt(keyPrivate3, cypher3); // decrypt  
+            System.Diagnostics.Debug.Assert(plain3 == "programmer 3");
 
 
         }
