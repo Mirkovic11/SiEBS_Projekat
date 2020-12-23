a4023afd6b849b4a0986ba2127a19bacb43bf033
Uros981 <urosristovic23@gmail.com>
2020-12-23 23:34:12 +0100

    Radiii

---
+++ SBESProjekat/WCFService/ServiceProxy.cs
@@ -0,0 +1,43 @@
+﻿using Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCFService
+{
+    public class ServiceProxy : ChannelFactory<ICertificateManager>, ICertificateManager, IDisposable
+    {
+        ICertificateManager factory;
+
+        public ServiceProxy(NetTcpBinding binding, string address) : base(binding, address)
+        {
+            factory = this.CreateChannel();
+        }
+
+        public ServiceProxy(NetTcpBinding binding, EndpointAddress address) : base(binding, address)
+        {
+
+            factory = this.CreateChannel();
+            //zabraniti izvrsavanje autentifikacije putem NTLM protokola
+            //Credentials.Windows.AllowNtlm = false;
+        }
+
+        public void createCertificateWithallKeys(string trustedRootName, string certificateName)
+        {
+            factory.createCertificateWithallKeys(trustedRootName, certificateName);
+        }
+
+        public void createCertificateWithoutPrivateKey(string trustedRootName, string certificateName)
+        {
+            factory.createCertificateWithoutPrivateKey(trustedRootName, certificateName);
+        }
+
+        public void createTrustedRootCA(string trustedRootName)
+        {
+            factory.createTrustedRootCA(trustedRootName);
+        }
+    }
+}
